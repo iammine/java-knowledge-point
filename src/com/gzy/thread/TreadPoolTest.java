4b1ab7b5259d86a3eef76ad38170f6f5a11b0050
iammine <guo_ze_yuan@126.com>
2015-08-22 17:23:00 +0800

    modify thread pool test

--- src/com/gzy/thread/TreadPoolTest.java
+++ src/com/gzy/thread/TreadPoolTest.java
@@ -1,5 +1,8 @@
 package com.gzy.thread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Title:TreadPoolTest
  * @Description:
@@ -12,8 +15,49 @@ package com.gzy.thread;
 public class TreadPoolTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// cachedTreadPool
+		ExecutorService executor = Executors.newCachedThreadPool();
+		
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				System.out.print("thread in cach thread pool run");
+			}
+		});
+		
+		// fixedthreadpool
+		ExecutorService fixedExecutor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 10);
+		fixedExecutor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				System.out.println("thread in fix thread pool run");
+			}
+		});
+		
+		// singlethreadpool
+		ExecutorService singleExecutor = Executors.newSingleThreadExecutor();
+		singleExecutor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				System.out.println("thread in single thread pool run");
+			}
+		});
+		
+		// scheduledthreadpool
+		ExecutorService scheduledExecutor = Executors.newScheduledThreadPool(10);
+		scheduledExecutor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				System.out.println("thread in scheduled pool run");
+			}
+		});
 	}
 
+	
 }
